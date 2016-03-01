26aed553a6115503198bfda26fb82169fbe1a7b4
Anthony Garo <agaro1121@gmail.com>
2016-02-29 22:04:41 -0500

    .

--- src/main/scala/Combinator3.scala
+++ src/main/scala/Combinator3.scala
@@ -1,18 +1,21 @@
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
-import scala.util.{Failure, Success}
+import Operations.errorResult
 
 
-object Callback2 {
+object Combinator3 {
 
   def main(args: Array[String]) {
 
     val codes = Future{ Thread.sleep(2000); "Cheats for Mortal Kombat Received!\nUp, Up, Down, Down, Left, Right, A, B, A\nEveryone wants kool stuff :-)"}
 
-      codes onComplete {
-        case Success(something) => println(something)
-        case Failure(error) => println("Error ->" + error.getMessage)
-      }
+    codes onSuccess{
+      case something => println(something)
+    }
+
+    codes onFailure {
+      case error => println("There was an error")
+    }
 
     scala.io.StdIn.readLine()
   }
